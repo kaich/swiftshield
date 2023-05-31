0ceae47118d043a7b48028fe6bcd04bea9605ec6
Artem Tverdokhliebov <artem.tverdokhlebov@gmail.com>
2023-05-31 14:00:54 +0300

    Update ViewController.swift

--- ExampleProject/ExampleProject/ViewController.swift
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -78,11 +78,28 @@ class DataManager {
     }
 }
 
-// MARK: Creating
-
 extension DataManager {
     func creatingContact() {
         let contact = TestEntity(context: persistentContainer.viewContext)
         contact.testAttribute = "test"
+    }
+}
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            if let optional = newValue as? AnyOptional, optional.isNil {
+                UserDefaults.standard.removeObject(forKey: key)
+            } else {
+                UserDefaults.standard.set(newValue, forKey: key)
+            }
+        }
     }
 }
