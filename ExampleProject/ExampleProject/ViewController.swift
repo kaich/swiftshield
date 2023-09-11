ddbb3499b88d9b26fba8d43e181de581f82408e9
kai <chengkai1853@163.com>
2023-09-11 11:29:59 +0800

    ..

--- ExampleProject/ExampleProject/ViewController.swift
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -1,6 +1,5 @@
 import DifferentModule
 import UIKit
-import CoreData
 
 func globalMethod() {}
 var globalProp = 0
@@ -35,8 +34,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .green
         StructFromDifferentModule.methodFromDifferentModule()
-        
-        
     }
 
     func method(_: SomeEnum) {
@@ -48,54 +45,5 @@ class ViewController: UIViewController {
         method(SomeEnum.b)
         method(SomeEnum.c)
         globalMethod()
-    }
-}
-
-class DataManager {
-    static let shared = DataManager()
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Model")
-        container.loadPersistentStores(completionHandler: { _, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // Core Data Saving support
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-}
-
-extension DataManager {
-    func creatingContact() {
-        let contact = TestEntity(context: persistentContainer.viewContext)
-        contact.testAttribute = "test"
-    }
-}
-
-@propertyWrapper
-struct UserDefault<T> {
-    let key: String
-    let defaultValue: T
-
-    var wrappedValue: T {
-        get {
-            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: key)
-        }
     }
 }
